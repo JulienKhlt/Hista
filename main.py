486876096ee47ac1de8c15500a92103686ab6522
pierrelouis <ruhlmannpl@gmail.com>
2021-04-05 12:07:40 +0200

    Add plotting of eta

--- main.py
+++ main.py
@@ -1,29 +1,57 @@
 from math import sqrt
 import numpy as np
-from random import randint, random, normal
+from random import randint, random, normalvariate
+import matplotlib.pyplot as plt
 
-δ = 0.64
-σ = sqrt(0.2)
+delta = 0.64
+sigma = sqrt(0.2)
+alpha = 0
 
-def η(u, t):
-    return np.sign(u) * max(abs(u) - t, 0)
+
+def eta(u, t):
+    return np.sign(u) * np.maximum(np.abs(u) - t, 0)
+
 
 def r():
-    if (randint(0, 1) == 1):
+    if randint(0, 1) == 1:
         return 1
-    else :
+    else:
         return -1
 
+
 def B(p):
-    if (random() > p):
+    if random() > p:
         return 1
-    else :
+    else:
         return 0
+
+
+def resoudre_equation(a):
+    pass
 
 
 def compute_esperance(nb, p):
     for i in range(nb):
-        θ = r() * B(p)
-        Z = normal()
-        resoudre_equation(α, )
-        
+        theta = r() * B(p)
+        Z = normalvariate(0, 1)
+        resoudre_equation(alpha)
+
+
+def plot_eta(t_values=None):
+    if t_values is None:
+        t_values = [1, 2, 3]
+    fig, (ax0, ax1, ax2) = plt.subplots(nrows=1, ncols=3, figsize=(14, 6))
+    mu = np.linspace(-5, 5, 50)
+
+    ax0.set_title(r"$\eta(\mu,t)$ with $t=1$")
+    ax0.plot(mu, eta(mu, t_values[0]))
+
+    ax1.set_title(r"$\eta(\mu,t)$ with $t=2$")
+    ax1.plot(mu, eta(mu, t_values[1]))
+
+    ax2.set_title(r"$\eta(\mu,t)$ with $t=3$")
+    ax2.plot(mu, eta(mu, t_values[2]))
+
+    fig.suptitle(r"Plotting of $\eta(\mu, t)$ for $t=1,2,3$")
+    plt.show()
+    return
